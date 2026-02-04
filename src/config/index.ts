c7ad30167ffe629607ec63f88facef0bd2e5eb41
Behruzbee <behruz.baxtiyorov.dev@gmail.com>
2026-02-04 16:12:42 +0500

    main

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 if (!process.env.BOT_TOKEN) throw new Error("❌ BOT_TOKEN is missing");
-if (!process.env.MONGODB_URI) throw new Error("❌ MONGODB_URI is missing");
+if (!process.env.rayxona_MONGODB_URI) throw new Error("❌ rayxona_MONGODB_URI is missing");
 
 const sourceGroupId = Number(process.env.SOURCE_GROUP_ID);
 
@@ -11,13 +11,16 @@ export const TARGET_GROUPS = [
     { id: -1003411289776, name: "SMM LID Parkent", limit: 25 },
     { id: -1003573376730, name: "SMM LID Yangi bozor", limit: 10 },
     { id: -1003512729243, name: "SMM LID Юнусабад", limit: 20 },
+    { id: -5171942403, name: "SMM LID Nasima bonu", limit: 0 },
+    { id: -5101616619, name: "SMM LID Fotima Sulton", limit: 10 },
+    { id: -5192989576, name: "SMM LID Oqqurg'on", limit: 0 },
 ];
 
 const targetIds = TARGET_GROUPS.map(g => g.id);
 
 export const config = {
     botToken: process.env.BOT_TOKEN,
-    mongoUri: process.env.MONGODB_URI,
+    mongoUri: process.env.rayxona_MONGODB_URI,
     sourceGroupId: sourceGroupId,
     targetGroups: targetIds, // Список ID для балансировщика
     groupsConfig: TARGET_GROUPS, // Полный конфиг с лимитами
