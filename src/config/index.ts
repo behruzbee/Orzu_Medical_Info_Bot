458bd77ed3c9934a058a72b4982151b6aa4d1f6a
Behruzbee <behruz.baxtiyorov.dev@gmail.com>
2026-02-05 14:42:15 +0500

    main

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,9 +11,9 @@ export const TARGET_GROUPS = [
     { id: -1003411289776, name: "SMM LID Parkent", limit: 25 },
     { id: -1003573376730, name: "SMM LID Yangi bozor", limit: 10 },
     { id: -1003512729243, name: "SMM LID Юнусабад", limit: 20 },
-    { id: -5171942403, name: "SMM LID Nasima bonu", limit: 0 },
+    // { id: -5171942403, name: "SMM LID Nasima bonu", limit: 0 },
     { id: -5101616619, name: "SMM LID Fotima Sulton", limit: 10 },
-    { id: -5192989576, name: "SMM LID Oqqurg'on", limit: 0 },
+    // { id: -5192989576, name: "SMM LID Oqqurg'on", limit: 0 },
 ];
 
 const targetIds = TARGET_GROUPS.map(g => g.id);
