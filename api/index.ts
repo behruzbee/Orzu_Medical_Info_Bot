346a025951f0339a889815dcf706d1271ef39c4c
Behruzbee <behruz.baxtiyorov.dev@gmail.com>
2026-03-05 12:37:15 +0500

    main

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,14 @@
 import { webhookCallback } from "grammy";
-import { bot } from "../src/bot";
+import { bot } from "../src/bot"; // Убедитесь, что путь к bot.ts правильный
 
-export default webhookCallback(bot, "http");
+const handleUpdate = webhookCallback(bot, "http");
+
+// @ts-ignore
+export default async function (req, res) {
+    try {
+        await handleUpdate(req, res);
+    } catch (err) {
+        console.error("Webhook Error:", err);
+        res.status(500).send("Error");
+    }
+}
